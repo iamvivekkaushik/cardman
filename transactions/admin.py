d794b4185f1c6b422a77c67611a86cb1fd9d11da
Vivek Kaushik <me@vivekkaushik.com>
2023-07-16 15:43:52 +0530

    [Updated] admin view, added cvv, expiry and name

--- transactions/admin.py
+++ transactions/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 
 from .models import Transaction
 
-admin.site.register(Transaction)
+
+class TransactionsAdmin(admin.ModelAdmin):
+    list_display = ('amount_in_paise', 'transaction_type', 'merchant', 'card', 'time')
+    list_filter = ('card__card_number', 'card__bank__name', 'transaction_type')
+
+
+admin.site.register(Transaction, TransactionsAdmin)
