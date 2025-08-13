eca90f2155965cb4be121f4629917ee4575a0df7
Vivek Kaushik <me@vivekkaushik.com>
2025-08-14 00:52:25 +0530

    [Added] filters to card list api

--- cards/views.py
+++ cards/views.py
@@ -1,8 +1,18 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.generics import ListAPIView, CreateAPIView
 from .serializers import *
 
 
 class CardListView(ListAPIView):
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = {
+        'bank_name': ['exact'],
+        'card_number': ['exact', 'endswith', 'startswith'],
+        'account_type': ['exact'],
+        'is_active': ['exact'],
+        'is_paid': ['exact'],
+    }
+    
     def get_queryset(self):
         user = self.request.user
         return Card.objects.filter(user=user)
